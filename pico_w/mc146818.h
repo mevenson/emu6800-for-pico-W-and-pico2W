393f926768ad7bb35ec0e13326d41b8212a3c230
Michael Evenson <michael.evenson@flexusergroup.com>
2025-03-16 06:36:59 -0400

    Add files via upload

--- pico_w/mc146818.h
+++ pico_w/mc146818.h
@@ -1,12 +1,12 @@
-typedef struct {
-    int16_t year;    ///< 0..4095
-    int8_t month;    ///< 1..12, 1 is January
-    int8_t day;      ///< 1..28,29,30,31 depending on month
-    int8_t dotw;     ///< 0..6, 0 is Sunday
-    int8_t hour;     ///< 0..23
-    int8_t min;      ///< 0..59
-    int8_t sec;      ///< 0..59
-} datetime_t;
+// typedef struct {
+//     int16_t year;    ///< 0..4095
+//     int8_t month;    ///< 1..12, 1 is January
+//     int8_t day;      ///< 1..28,29,30,31 depending on month
+//     int8_t dotw;     ///< 0..6, 0 is Sunday
+//     int8_t hour;     ///< 0..23
+//     int8_t min;      ///< 0..59
+//     int8_t sec;      ///< 0..59
+// } datetime_t;
 
 #ifndef MC146818
     extern bool got_time;
